ba7824389bfe7670818176adeefeb9dcb0001454
Brendan C. Ward <bcward@astutespruce.com>
2024-10-01 15:40:15 -0700

    Switch from poetry to uv

--- Developing.md
+++ Developing.md
@@ -10,5 +10,18 @@ The API is implemented in Python and provides summary reports for pre-defined su
 
 ## Development
 
-Python dependencies are managed using `poetry`. First, install poetry, then
-`poetry install` to install most of them.
+Python dependencies are managed using `uv`. First,
+[install uv](https://docs.astral.sh/uv/), then:
+
+```bash
+uv venv .venv --python 3.12
+<source it according to your shell, e.g., source .venv/bin/activate.fish>
+uv sync --extras dev
+```
+
+To update the requirements.txt file used to build these dependencies into the API
+Docker container for deployment, run:
+
+```bash
+uv pip compile pyproject.toml -o ../secas-docker/docker/api/secas-ssa-requirements.txt
+```
--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
-
-description = "SECAS Southeast Species Status Landscape Assessment Tool"
-
-if os.path.exists("README.md"):
-    long_description = open("README.md").read()
-else:
-    long_description = description
 
 setup(
-    name="SECAS Southeast Species Status Landscape Assessment Tool",
-    version="0.2.0",
-    url="https://github.com/astutespruce/secas-ssa",
-    license="MIT",
-    author="Brendan C. Ward",
-    author_email="bcward@astutespruce.com",
-    packages=["analysis", "api"],
-    description=description,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
-    setup_requires=["numpy"],
-    install_requires=["numpy>=1.23"],
-    python_requires=">=3.10",
+    packages=find_packages(),
 )
