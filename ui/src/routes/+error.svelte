2a9c596a23ecf71756e3f4b34222592b81599bc3
Brendan C. Ward <bcward@astutespruce.com>
2026-01-05 18:03:36 -0800

    Cleanup display of image credits on error page

--- ui/src/routes/+error.svelte
+++ ui/src/routes/+error.svelte
@@ -16,23 +16,25 @@
 </svelte:head>
 
 {#if page.status === 404}
-	<div class="relative flex-auto h-[100%]">
+	<div class="relative flex-auto h-full">
 		<div class="hidden md:block absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
 			<enhanced:img src="$images/48754428566_d34b348ac3_o.jpg" alt="" class="brightness-80" />
+			<div class="sticky bottom-0">
+				<div class="absolute bottom-0 right-0 bg-black/65 text-white px-2 text-sm">
+					Photo:
+					<a
+						href="https://flickr.com/photos/usfwssoutheast/48754428566/"
+						target="_blank"
+						class="text-white"
+					>
+						G. Peeples / U.S. Fish and Wildlife Service Southeast Region</a
+					>
+				</div>
+			</div>
 		</div>
-		<div class="relative mx-auto max-w-[800px] p-4 bg-white top-10">
+		<div class="relative mx-auto max-w-200 p-4 bg-white top-10">
 			<h1 class="text-5xl">PAGE NOT FOUND</h1>
 			<h2 class="text-3xl">Sorry, we could not find what you were looking for here.</h2>
-		</div>
-		<div class="absolute bottom-0 right-0 bg-black/65 text-white px-2 text-sm">
-			Photo:
-			<a
-				href="https://flickr.com/photos/usfwssoutheast/48754428566/"
-				target="_blank"
-				class="text-white"
-			>
-				G. Peeples / U.S. Fish and Wildlife Service Southeast Region</a
-			>
 		</div>
 	</div>
 {:else}
